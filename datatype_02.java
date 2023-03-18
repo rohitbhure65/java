61ba467602b9156e32e30a7a8003162f841ba63f
rohit <rohit@kali>
2023-03-18 16:58:50 +0530

    datatypemodifiy

--- datatype_02.java
+++ datatype_02.java
@@ -2,11 +2,11 @@ class datatype_02 {
     public static void main(String[] args){
         //variables
         int age = 42;
-        long mobile = 00000000430320;
+        long mobile = 34430320;
         char section = 'A';
         byte num = 43;
-        float a = 42.4;
-        double b = 443d;
+        float a = 42.4f;
+        double b = 44.3d;
         String name = "Rohit bhure";
         boolean present = true;
     }
