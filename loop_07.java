4311ca321c78b94d8f5396ed88bd81801904b1d7
rohit <rohit@kali>
2023-03-20 14:24:49 +0530

    loop

---
+++ loop_07.java
@@ -0,0 +1,10 @@
+public class loop_07 {
+    public static void main(String[] args) {
+        // counter++ => counter = counter + 1
+
+        // for loop
+        for(int counter = 0; counter <= 10; counter++){
+            System.out.println(counter);
+        }
+    }
+}
