483f5ef280791da3d2a253430319b43b1893f0da
rohit <rohit@kali>
2023-03-30 15:46:14 +0530

    array complte :)

--- array_10.java
+++ array_10.java
@@ -22,5 +22,31 @@ public class array_10{
             System.out.println(marks[i]);
         }
 
+        // ---------------------------------
+
+        // well define array size & variable
+        int roll[] = {30,31,32,33,34,35};
+
+        for (int i = 0; i < roll.length; i++) {
+            System.out.println(roll[i]);
+        }
+
+        // ------------------------------------
+
+        // set array size with variables
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt(); // input 10 memory allocate 10 x 4 bytes and store null value 0 if array type was integer if array type was flote then they store 0.0 as null value
+        int[] phone = new int[size]; // size = 10
+
+        // intput
+        for (int i = 0; i < size; i++) {
+            phone[i] = sc.nextInt(); // enter 10 numbers as a input
+        }
+
+        // output
+        for (int i = 0; i < size; i++) {
+            System.out.println(phone[i]); // print 10 numbers as a output
+        }
+
     }
 }
